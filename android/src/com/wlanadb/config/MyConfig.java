e8c54476dc8a6f533bd0a11b1818ba34cd2516f1
Stepan Rudenko <stepan@layar.com>
2012-11-14 02:46:00 +0200

    ANDROID: copy text fixed

--- android/src/com/wlanadb/config/MyConfig.java
+++ android/src/com/wlanadb/config/MyConfig.java
@@ -1,7 +1,7 @@
 package com.wlanadb.config;
 
 public class MyConfig {
-  public static final boolean DEBUG = true;
+  public static final boolean DEBUG = false;
 
   public static final String VERSION = "0.1b";
   public static final String REVISION = "41bbc57a5c2b";
