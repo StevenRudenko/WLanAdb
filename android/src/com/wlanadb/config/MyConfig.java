8556d86c1ae7d6ac2ea5c3255d3fb81e780b6aaf
Stepan Rudenko <stepan@layar.com>
2012-11-14 04:06:12 +0200

    ANDROID: revision was set for v0.1b

--- android/src/com/wlanadb/config/MyConfig.java
+++ android/src/com/wlanadb/config/MyConfig.java
@@ -4,7 +4,7 @@ public class MyConfig {
   public static final boolean DEBUG = false;
 
   public static final String VERSION = "0.1b";
-  public static final String REVISION = "41bbc57a5c2b";
+  public static final String REVISION = "579820765028";
 
   public static final String GOOGLE_ANALITYCS_TRACKING_ID = "UA-35419273-1";
 }
