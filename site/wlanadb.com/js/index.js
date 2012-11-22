33f96be78d8dc7bbe6eb406ca6caf418d21d8093
Stepan Rudenko <stepan@layar.com>
2012-11-22 03:36:17 +0200

    SITE: - bug with scrollbar on home screen was fixed

--- site/wlanadb.com/js/index.js
+++ site/wlanadb.com/js/index.js
@@ -6,10 +6,6 @@ function changeContent(newContent) {
 	document.getElementById("content").setAttribute('src', newContent);
 }
 
-function onDownload(link) {
-	_gaq.push(['_trackEvent', 'Downloads', 'Download desktop client', 'Link <' + text($(link).html()) + '>']);
-}
-
 setInterval(function() {
 	var today = new Date();
 	document.getElementById('time_block').innerHTML = today.toLocaleTimeString().replace(/(.+)(\:\d{2})/, ' $1');
