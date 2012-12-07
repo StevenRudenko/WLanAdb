67a6959b36580ba9f68100e21e635a66d672182c
Stepan Rudenko <stepan@layar.com>
2012-12-07 12:27:07 +0200

    - changeset was set

--- android/src/com/wlanadb/config/MyConfig.java
+++ android/src/com/wlanadb/config/MyConfig.java
@@ -4,7 +4,7 @@ public class MyConfig {
   public static final boolean DEBUG = false;
 
   public static final String VERSION = "0.2b";
-  public static final String REVISION = "4e57ac62d360";
+  public static final String REVISION = "f9d78c79c6bc";
 
   public static final String GOOGLE_ANALITYCS_TRACKING_ID = "UA-35419273-1";
 }
