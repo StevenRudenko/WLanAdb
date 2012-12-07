1b7123a0f6bb1d954b555949b01647f24a86f492
Stepan Rudenko <stepan@layar.com>
2012-12-07 12:25:47 +0200

    - version 0.2b created

--- android/src/com/wlanadb/config/MyConfig.java
+++ android/src/com/wlanadb/config/MyConfig.java
@@ -1,9 +1,9 @@
 package com.wlanadb.config;
 
 public class MyConfig {
-  public static final boolean DEBUG = true;
+  public static final boolean DEBUG = false;
 
-  public static final String VERSION = "0.1b";
+  public static final String VERSION = "0.2b";
   public static final String REVISION = "4e57ac62d360";
 
   public static final String GOOGLE_ANALITYCS_TRACKING_ID = "UA-35419273-1";
